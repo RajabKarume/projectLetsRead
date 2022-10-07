22247505d6297f02bc41935ad2760d9656ab76e9
Rajab <rkarume@gmail.com>
2022-10-07 12:37:29 +0300

    deploy

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,8 +1,4 @@
 import { Carousel, Image } from 'antd';
-import image1 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/elisa-calvet-b-S3nUOqDmUvc-unsplash.jpg';
-import image2 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/fabiola-penalba--kl_XvEOqMU-unsplash.jpg';
-import image3 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/fallon-michael-qmlGWIaIgpo-unsplash.jpg';
-import image4 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/natalie-grainger-Mw1efRU1qcU-unsplash.jpg';
 import React from 'react';
 const contentStyle = {
   height: '500px',
@@ -16,17 +12,17 @@ const Homebanner = () => (
   <Carousel autoplay>
     <div>
       <h3  style={contentStyle}>
-        <Image src={image1}/>
+        <Image src="https://images.unsplash.com/photo-1520467795206-62e33627e6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJlYWRpbmclMjBib29rc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60"/>
       </h3>
     </div>
     <div>
-      <h3 style={contentStyle}><Image src={image2}/></h3>
+      <h3 style={contentStyle}><Image src="https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVhZGluZyUyMGJvb2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"/></h3>
     </div>
     <div>
-      <h3 style={contentStyle}><Image src={image3}/></h3>
+      <h3 style={contentStyle}><Image src="https://images.unsplash.com/photo-1522008342704-6b265b543c37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVhZGluZyUyMGJvb2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" /></h3>
     </div>
     <div>
-      <h3 style={contentStyle}><Image src={image4}/></h3>
+      <h3 style={contentStyle}><Image src="https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cmVhZGluZyUyMGJvb2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" /></h3>
     </div>
   </Carousel>
 );
