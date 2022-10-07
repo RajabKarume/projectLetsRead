df49d05b7405f2dc63bada9fcef7e7903769a967
Rajab <rkarume@gmail.com>
2022-10-07 10:24:09 +0300

    trying to deploy

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,8 +1,8 @@
 import { Carousel, Image } from 'antd';
-import image1 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/Images/elisa-calvet-b-S3nUOqDmUvc-unsplash.jpg';
-import image2 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/Images/fabiola-penalba--kl_XvEOqMU-unsplash.jpg';
-import image3 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/Images/fallon-michael-qmlGWIaIgpo-unsplash.jpg';
-import image4 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/Images/natalie-grainger-Mw1efRU1qcU-unsplash.jpg';
+import image1 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/elisa-calvet-b-S3nUOqDmUvc-unsplash.jpg';
+import image2 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/fabiola-penalba--kl_XvEOqMU-unsplash.jpg';
+import image3 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/fallon-michael-qmlGWIaIgpo-unsplash.jpg';
+import image4 from '/home/rajab/development/Code/Phase2/Final Project/my-app/src/src/Images/natalie-grainger-Mw1efRU1qcU-unsplash.jpg';
 import React from 'react';
 const contentStyle = {
   height: '500px',
